ec594f62e12ac5dbd7882916f21ddae46dec1d4e
larynx23 <91066887+larynx23@users.noreply.github.com>
2021-12-11 22:13:37 +0100

    Add files via upload

--- 3_fordulo/megoldas/Eseménykezelés haladó szinten/3_feladat/script.js
+++ 3_fordulo/megoldas/Eseménykezelés haladó szinten/3_feladat/script.js
@@ -8,59 +8,63 @@ let ctx = canvas.getContext('2d');
 
 let kovek = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600,
     650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150,
-    1200, 1250, 1300, 1350, 1400, 1450, 1500,]
+    1200, 1250, 1300, 1350, 1400, 1450, 1500];
+
+let miciW = 0
+
+mennyi = Math.floor((Math.random() * 20) + 20);
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+let ko = new Image();
+ko.src = 'kepek/ko.png';
+
+let mez = new Image();
+mez.src = 'kepek/mez.png';
+
+let mici = new Image();
+mici.src = 'kepek/micimacko.png';
+
+let kanga = new Image();
+kanga.src = 'kepek/kanga.png';
 
 
-mennyi = Math.floor((Math.random() * 20) + 20)
+szamok = [];
 
+ko.onload = function () {
 
-
-function betolt() {
-    let ko = new Image()
-    ko.src = 'kepek/ko.png'
-
-    let mez = new Image()
-    mez.src = 'kepek/mez.png'
-
-    szamok = []
-
-    ko.onload = function () {
-
-        // kövek
-        for (let i = 0; i < mennyi; i++) {
-            let index = Math.floor((Math.random() * kovek.length))
-            szamok.push(index)
-        }
-        uniq = szamok.filter(function (item, pos) {
-            return szamok.indexOf(item) == pos;
-        })
-        uniq.sort(function (a, b) { return a - b })
-
-        for (let i = 0; i < mennyi; i++) {
-            if (i % 2 == 0) {
-                ctx.drawImage(ko, kovek[uniq[i]], 250, 75, 75)
-            } else {
-                ctx.drawImage(ko, kovek[uniq[i]], 300, 75, 75)
-            }
-        }
-
-        // mézes csuprok
-        mezek_szama = Math.floor(uniq.length / 3)
-        console.log(uniq.length, mezek_szama)
-
-        for (i = 0; i < uniq.length; i++) {
-            for (_ = 0; _ < mezek_szama; _++) {
-                mez_w = Math.floor((Math.random() * 50) + kovek[uniq[i]])
-
-                if (i % 2 == 0) {
-                    mez_h = Math.floor((Math.random() * 50) + 250)
-                    ctx.drawImage(mez, mez_w, mez_h, 20, 20)
-
-                } else {
-                    mez_h = Math.floor((Math.random() * 50) + 300)
-                    ctx.drawImage(mez, mez_w, mez_h, 20, 20)
-                }
-            }
-        }
+    for (let i = 0; i < mennyi; i++) {
+        let index = Math.floor((Math.random() * kovek.length));
+        szamok.push(index);
     }
+    for (let i = 0; i < mennyi; i++) {
+        ctx.drawImage(ko, kovek[szamok[i]], 250, 50, 50);
+    }
+}
+mez.onload = function () {
+
+    szamok = szamok.filter(onlyUnique); // kitörli a sokszorosokat
+    for (let i = 0; i < Math.floor(szamok.length / 3); i++) {
+        ctx.drawImage(mez, (szamok[i] * 50) + 65, 240, 20, 20);
+    }
+
+    szamok.sort(function (a, b) { return a - b });
+    ctx.beginPath();
+    ctx.arc((szamok[Math.floor(szamok.length / 2)] * 50) + 75, 220, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+mici.onload = function () {
+    ctx.drawImage(mici, miciW, 230, 50, 50)
+}
+
+kanga.onload = function () {
+    ctx.drawImage(kanga, 1550, 210, 50, 70)
+}
+function miciLep(num) {
+    ctx.clearRect(miciW, 230, miciW + 50, 280)
+    miciW = kovek[szamok[num]]
+    ctx.drawImage(mici, miciW, 230, 50, 50)
 }
