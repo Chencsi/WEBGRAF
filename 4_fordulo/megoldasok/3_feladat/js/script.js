17db04262834cfc5410e25cc0132f68637a4d7b9
Kevin Chen <Kevinkeeeh@gmail.com>
2021-12-18 23:09:29 +0100

    fejlesztett 3. feladat

--- 4_fordulo/megoldasok/3_feladat/js/script.js
+++ 4_fordulo/megoldasok/3_feladat/js/script.js
@@ -1,13 +1,28 @@
-let numbers = []
-for (let i = 0; i < 10; i++) {
-    let rndNum = Math.floor(Math.random() * 10);
-    //if (rndNum == 0) rndNum = 1;
-    (numbers.includes(rndNum)) ? i -= 1 : numbers.push(rndNum);
+window.onload = function () {
+    let numbers = []
+    let hNumbers = [];
+    const tdInTable = document.querySelectorAll('td');
+
+    function addToInput(n){
+        if (hNumbers != 4){
+            hNumbers.push(n);
+            document.querySelector('#input').innerHTML = '*';
+        } else {
+            //check
+        }
+        console.log(hNumbers)
+    }
+
+    for (let i = 0; i < 10; i++) {
+        let rndNum = Math.floor(Math.random() * 10);
+        (numbers.includes(rndNum)) ? i -= 1 : numbers.push(rndNum);
+    }
+    for (let i = 0; i < 10; i++) {
+        document.getElementById(`button${i}`).innerHTML = numbers[i]
+    }
+    for (let i = 0; i < tdInTable.length; i++) {
+        tdInTable[i].addEventListener('click',function(){
+            addToInput(numbers[i+1])
+        });
+    }
 }
-console.log(numbers);
-for (let i = 0; i < 10; i++) {
-    document.getElementById(`button${i}`).innerHTML = numbers[i]
-}
-document.querySelectorAll('td','td').addEventListener('click',function(){
-    alert('asd')
-});
