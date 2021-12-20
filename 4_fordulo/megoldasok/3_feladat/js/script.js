657f8e3134d96e99a9719ef71c03e3583e06fab7
Kevin Chen <65046535+Chencsi@users.noreply.github.com>
2021-12-20 09:04:08 +0100

    Add files via upload

--- 4_fordulo/megoldasok/3_feladat/js/script.js
+++ 4_fordulo/megoldasok/3_feladat/js/script.js
@@ -1,16 +1,24 @@
 window.onload = function () {
-    let numbers = []
+    let numbers = [];
     let hNumbers = [];
     const tdInTable = document.querySelectorAll('td');
 
-    function addToInput(n){
-        if (hNumbers != 4){
+    function openFunction(){
+        document.getElementById('doorImg').style.backgroundImage = "url('../img/door.jpg')";
+    }
+
+    function addToInput(n) {
+        if (hNumbers.length != 4) {
             hNumbers.push(n);
-            document.querySelector('#input').innerHTML = '*';
-        } else {
-            //check
+            document.querySelector('#input').innerHTML =
+                document.querySelector('#input').innerHTML + '*';
+        } if (hNumbers.length == 4) {
+            setTimeout(function () {
+                (hNumbers.toString() == '8,9,9,6') ? openFunction() : alert('Próbáld újra!');
+                /*document.querySelector('#input').innerHTML = '';
+                hNumbers = [];*/
+            }, 1);
         }
-        console.log(hNumbers)
     }
 
     for (let i = 0; i < 10; i++) {
@@ -18,11 +26,18 @@ window.onload = function () {
         (numbers.includes(rndNum)) ? i -= 1 : numbers.push(rndNum);
     }
     for (let i = 0; i < 10; i++) {
-        document.getElementById(`button${i}`).innerHTML = numbers[i]
+        document.getElementById(`button${i}`).innerHTML = numbers[i];
     }
     for (let i = 0; i < tdInTable.length; i++) {
-        tdInTable[i].addEventListener('click',function(){
-            addToInput(numbers[i+1])
-        });
+        if (i == 10) {
+            tdInTable[10].addEventListener('click', function () {
+                addToInput(numbers[i - 10])
+            });
+        } else {
+            tdInTable[i].addEventListener('click', function () {
+                addToInput(numbers[i + 1]);
+            });
+        }
+
     }
 }
