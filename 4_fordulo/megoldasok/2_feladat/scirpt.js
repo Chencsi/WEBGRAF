2fc7e42ac0153b97dbba3c7261616368155161b3
larynx23 <91066887+larynx23@users.noreply.github.com>
2021-12-21 17:29:27 +0100

    Add files via upload

--- 4_fordulo/megoldasok/2_feladat/scirpt.js
+++ 4_fordulo/megoldasok/2_feladat/scirpt.js
@@ -1,23 +1,66 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let FPS = 60
+let FPS = 60;
 
-let hossz = 880
-let ero = 10
-let szin = 'red'
+let kint = true
+let mark = new Image()
+mark.src = 'img/markolat.png'
+
+let hossz = document.getElementById('hossz').value;
+let ero = document.getElementById('ero').value;
+let szin = document.getElementById('szin').value;
 
 function sub() {
-    hossz = document.getElementById('hossz')
-    ero = document.getElementById('ero')
-    szin = document.getElementById('szin')
+    hossz = document.getElementById('hossz').value;
+    ero = document.getElementById('ero').value;
+    szin = document.getElementById('szin').value;
 }
 
-function gameloop(){
+function kimegy() {
+    kint = !kint
+    if (kint) {
+        animation('ki')
+    } else {
+        animation('be')
+    }
+}
+
+function gameloop() {
+    if (kint) {
+        rajz(hossz);
+    } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(mark, 0, 150, 360, 200);
+    }
+
+    // document.getElementById('szin').style.backgroundColor = szin
+}
+
+function animation(merre) {
+    if (merre == 'be') {
+        for (let i = hossz; i > 350; i--) {
+            setTimeout(() => { rajz(i) }, 10);
+
+        }
+    } else if (merre == 'ki') {
+        for (let i = 350; i < hossz; i++) {
+            setTimeout(() => { rajz(i) }, 10);
+        }
+    }
+}
+setInterval(gameloop, 1000 / FPS);
+
+function rajz(hossz) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.moveTo(100, 250)
+    ctx.moveTo(340, 250);
     ctx.lineTo(hossz, 250);
     ctx.lineWidth = ero;
     ctx.strokeStyle = szin;
+    ctx.lineCap = 'round';
+    ctx.shadowBlur = '30';
+    ctx.shadowColor = szin;
     ctx.stroke();
+    ctx.shadowBlur = '0';
+    ctx.drawImage(mark, 0, 150, 360, 200);
 }
-setInterval(gameloop, 100)
